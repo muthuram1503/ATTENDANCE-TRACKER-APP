1b20d230b5d5fb18583e75fc930055ef74e6ddcf
muthuram1503 <mmuthuram41@gmail.com>
2025-08-27 15:16:43 +0530

    Keep vercel.json and resolve conflict

--- backend/lib/connect.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const connectDB = async () => {
-  if (mongoose.connection.readyState >= 1) return;
-
-  return mongoose.connect(process.env.MONGODB_URI); // make sure env is set
-};
-
-export default connectDB;
